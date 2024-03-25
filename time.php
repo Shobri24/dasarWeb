d0028dc1efb7bd9826e3e0166e2b704d85009da7
Shobri24 <shobri2424@gmail.com>
2024-03-25 08:02:19 +0700

    Pertemuan 5 - Date dan Time

---
+++ time.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+</head>
+
+<body>
+    <h3>Time</h3>
+    <?php
+    date_default_timezone_set("Asia/Jakarta");
+    echo date("h:i:sa");
+    ?>
+    <br>
+</body>
+
+</html>
